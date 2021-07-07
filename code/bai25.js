22d080b9f5ceb0ffe33124383d3672bdea994506
tmtuan154 <tmtuan154@gmail.com>
2021-07-07 10:57:54 +0700

    bai25.js

---
+++ code/bai25.js
@@ -0,0 +1,84 @@
+/**
+ * Tạo một đối tượng student gồm các property name, sex, age
+ * Example:
+  name: "CodersX",
+  sex: "Male",
+  age: 1
+ * Viết hàm showInfo trả về object đó
+*/
+
+var student = { name: "CodersX", sex: "Male", age: 1 };
+
+function showInfo(obj) {
+  return typeof obj;
+}
+
+/**
+ * Viết hàm xoá thuộc tính (prop) khỏi object student
+ * Trả về object đã được xoá
+ * Example:
+ *  removeProp("name") = { class: “VI”, rollno: 12 }
+ */
+
+/**
+ * Viết hàm xoá thuộc tính (prop) khỏi object student
+ * Trả về object đã được xoá
+ * Example:
+ *  removeProp("name") = { class: “VI”, rollno: 12 }
+*/
+
+function removeProp(prop) {
+    var student = {
+            name : "David Rayy",
+            class : "VI",
+            rollno : 12
+        }
+  // chỉ viết code ở dưới dòng này.
+  delete student[prop]
+  return student
+}
+
+
+/**
+ * Viết hàm countProperty để in ra độ dài của một đối tượng
+ * @param {object}
+ * @return {number}
+ * 
+ * Example: 
+ * var student = {
+ *      name: "Jerry", 
+ *      age: 14,
+ *      role: "student"
+ *  }
+ * lengthObj(student); // length: 3
+}
+*/
+
+function countProperty(obj) {
+  var count = 0;
+  for (item in obj)
+  {
+      count++;
+  }
+  return count;
+}
+
+
+/**
+ * Viết hàm xây dựng Shape để tạo các đối tượng hình học có các thuộc tính sau:
+ *  {
+ *    color: String,
+ *    shape: String,
+ *    fill: boolean
+ *  }
+*/
+
+function Shape(color, shape, fill) {
+  this.color = color;
+  this.shape = shape;
+  this.fill = fill;
+}
+
+var shape1 = new Shape("red", "rectangle", true);
+var shape2 = new Shape("orange", "triangle", false);
+var shape3 = new Shape("tomato", "circle", true);
